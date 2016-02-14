a1d03728dd4e2858fe17c4346078c584065ce2b9
Chris Greene <archgoon@gmail.com>
2016-02-14 15:46:20 -0800

    QUANTUM-1 Added unittest for utils.js
    The original version of this function was pretty damn broken. Yay
    unittests.
    
    We now have unittests for all the files.
    
    Fixes #1

--- utils.js
+++ utils.js
@@ -1,9 +1,12 @@
 var ArrayUtils = {}
 ArrayUtils.alteredIndex = function(array, index, replacement) {
-    return array.splice(0, index).concat([replacement]).concat(index+1, array.length);
+    return array.slice(0, index).concat([replacement]).concat(array.slice(index+1, array.length));
 }
 
 ArrayUtils.addedIndex = function(array, index, increment) {
-    return ArrayUtils.alterdIndex(array, index, array[index] + increment);
+    return ArrayUtils.alteredIndex(array, index, array[index] + increment);
 }
 
+ArrayUtils.equals = function(array1, array2) {
+    return JSON.stringify(array1) === JSON.stringify(array2)
+}
---
+++ utils.unittest.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+assert(ArrayUtils.equals(["1"],["1"]))
+assert(!ArrayUtils.equals(["1", "2"],["1"]))
+assert(ArrayUtils.equals(["1", "2"],["1","2"]))
+assert(!ArrayUtils.equals(["1", "2"],["1","4"]))
+assert(ArrayUtils.equals(["1", ["1","2"]],["1",["1","2"]]))
+assert(ArrayUtils.equals([1,["a","b","c"],3],
+    ArrayUtils.alteredIndex([1,2,3], 1, ["a", "b","c"])))
+assert(ArrayUtils.equals(ArrayUtils.addedIndex([1,2,3], 1, 1),
+    [1,3,3]))
